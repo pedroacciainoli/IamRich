f789523fef962b727f0c309f61fcdd5c2d643dc5
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-04-26 10:31:41 -0300

    🚀finalizado

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Pedro Acciainoli on 23/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
